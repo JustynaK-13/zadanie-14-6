a8688eb5c0e56a26ee258c4decbe872a9185244d
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-04-12 12:36:41 +0200

    Add decrement function

--- js/script.js
+++ js/script.js
@@ -10,7 +10,13 @@ var Counter = React.createClass({
             counter: this.state.counter + 1
         });
     },
-    
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
     render: function() {
         return React.createElement('div', {onClick: this.increment},
             React.createElement('span', {}, 'Licznik ' + this.state.counter)
